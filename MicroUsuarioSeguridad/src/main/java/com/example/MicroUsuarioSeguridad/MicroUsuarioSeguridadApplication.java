4f400038bb53d4f1298bf86ac5d02829ce5a150d
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:33:27 -0400

    Add files via upload

---
+++ MicroUsuarioSeguridad/src/main/java/com/example/MicroUsuarioSeguridad/MicroUsuarioSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroUsuarioSeguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUsuarioSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroUsuarioSeguridadApplication.class, args);
+	}
+
+}
---
+++ MicroUsuarioSeguridad/src/test/java/com/example/MicroUsuarioSeguridad/MicroUsuarioSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroUsuarioSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroUsuarioSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
